c816331e1fa96798cdfce2522c836db6bea86606
ChrFr <christoph.franke@web.de>
2019-01-09 13:20:36 +0100

    bootstrap theme

--- orcadjango/orcadjango/urls.py
+++ orcadjango/orcadjango/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('orca/', include('orcaserver.urls')),
+    path('', include('orcaserver.urls')),
 ]
