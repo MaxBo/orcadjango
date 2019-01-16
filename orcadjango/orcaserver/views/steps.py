045c848d19e7bb64afe1df36a36c196990d11226
MaxBo <max.bohnet@tuhh.de>
2019-01-16 23:23:05 +0100

    cast injectables to original types

--- orcadjango/orcaserver/views/steps.py
+++ orcadjango/orcaserver/views/steps.py
@@ -8,6 +8,7 @@ from orcaserver.models import Step, Injectable, Scenario
 from django.urls import reverse
 from threading import Thread
 import time
+import ast
 import contextlib
 import logging
 import json
@@ -18,7 +19,18 @@ def apply_injectables(scenario):
     names = orca.list_injectables()
     injectables = Injectable.objects.filter(name__in=names, scenario=scenario)
     for inj in injectables:
-        orca.add_injectable(inj.name, inj.value)
+        #  get original type of injectable value (if not available, use string)
+        original_type = type(orca._injectable_backup.get(inj.name, str))
+        # compare to evaluation or value
+        try:
+            converted_value = ast.literal_eval(inj.value)
+            if not isinstance(converted_value, original_type):
+                #  try to cast the value using the original type
+                converted_value = original_type(inj.value)
+        except (SyntaxError, ValueError):
+            #  if casting does not work, use string
+            converted_value = inj.value
+        orca.add_injectable(inj.name, converted_value)
 
 
 class StepsView(ScenarioMixin, TemplateView):
