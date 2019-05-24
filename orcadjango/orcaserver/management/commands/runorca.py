edf72cc82cff09356f9fb074223cf2fd33e658cb
MaxBo <bohnet@ggr-planung.de>
2019-05-24 15:37:51 +0200

    reload injectables from cascading imports

--- orcadjango/orcaserver/management/commands/runorca.py
+++ orcadjango/orcaserver/management/commands/runorca.py
@@ -3,14 +3,23 @@ import orca
 import importlib
 
 
-def load_module(module_name):
-    orca.clear_all()
+def load_module(module_name, module_set=None):
+    if module_set is None:
+        orca._python_module = module_name
+        orca.clear_all()
+        orca._injectable_backup = {}
+        module_set = {module_name}
     module = importlib.import_module(module_name)
     importlib.reload(module)
-    orca._python_module = module_name
-    orca._injectable_backup = {}
     for inj in orca.list_injectables():
         orca._injectable_backup[inj] = orca.get_injectable(inj)
+
+    if hasattr(module, '__parent_modules__'):
+        for module_name in module.__parent_modules__:
+            if not module_name in module_set:
+                load_module(module_name, module_set)
+                module_set.add(module_name)
+
 
 
 class Command(BaseCommand):
