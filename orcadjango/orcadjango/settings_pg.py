883e29982eb51e27f4898625045539861303352b
Christoph Franke <franke@ggr-planung.de>
2020-10-15 14:29:25 +0200

    environment settings

---
+++ orcadjango/orcadjango/settings_pg.py
@@ -0,0 +1,20 @@
+from orcadjango.settings import *
+
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASS = os.environ['DB_PASS']
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_PORT = os.environ.get('DB_PORT', '5432')
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASS,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+        'OPTIONS': {}
+    },
+}
