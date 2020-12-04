7d7dafd8bd765c458ed9e13aa607bf1f24926d7d
Christoph Franke <franke@ggr-planung.de>
2020-12-04 11:02:54 +0100

    style steps log and add btn

--- orcadjango/orcaserver/views/steps.py
+++ orcadjango/orcaserver/views/steps.py
@@ -75,7 +75,6 @@ class StepsView(ProjectMixin, TemplateView):
             step.valid = True
         kwargs = super().get_context_data(**kwargs)
         kwargs['steps_available'] = steps_grouped if scenario else []
-        kwargs['steps_count'] = len(steps_available)
         kwargs['steps_scenario'] = steps_scenario
         logs = LogEntry.objects.filter(scenario=scenario).order_by('-timestamp')
         kwargs['logs'] = logs
