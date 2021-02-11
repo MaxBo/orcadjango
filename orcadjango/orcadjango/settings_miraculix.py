3e906f683e3e3b6d3d0c87382729028e1c35eb4a
Christoph Franke <franke@ggr-planung.de>
2021-02-11 16:29:40 +0100

    added miraculix settings with timezone Berlin

---
+++ orcadjango/orcadjango/settings_miraculix.py
@@ -0,0 +1,17 @@
+from orcadjango.settings_pg import *
+
+TIMEZONE = 'Europe/Berlin'
+
+ORCA_MODULES['default'].update({'path': 'extractiontools.steps.extract_data'})
+
+ORCA_MODULES['available'].update({
+    'extraction tools': {
+        'path': 'extractiontools.steps.extract_data',
+        'description': ('collection of tools to extract data from the europe '
+                        'database to a project database'),
+        'init': ['database', 'target_srid', 'project_area']
+    }
+})
+
+ALLOWED_HOSTS = ['localhost',
+                 'miraculix.ggr-planung.de']
