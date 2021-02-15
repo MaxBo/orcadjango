111ec34a62865ac66d18af42ea160e5182b244b0
Christoph Franke <franke@ggr-planung.de>
2021-02-15 11:19:55 +0100

    removed old code from meta decorator

--- orcadjango/orcadjango/decorators.py
+++ orcadjango/orcadjango/decorators.py
@@ -1,21 +1,15 @@
 import orca
 
-
 def meta(**kwargs):
     """
     Decorates functions that will be injected into other functions.
     """
     def decorator(func):
         name = func.__name__
-        orcafunc_wrapper = orca._STEPS.get(
-            name, orca._injectable_function.get(
-                name, orca._INJECTABLES.get(name)))
-        if orcafunc_wrapper:
-            if not hasattr(orca, 'meta'):
-                orca.meta = {}
-            if name not in orca.meta:
-                orca.meta[name] = {}
-            for k, v in kwargs.items():
-                orca.meta[name][k] = v
-        return func
+        if not hasattr(orca, 'meta'):
+            orca.meta = {}
+        if name not in orca.meta:
+            orca.meta[name] = {}
+        for k, v in kwargs.items():
+            orca.meta[name][k] = v
     return decorator
