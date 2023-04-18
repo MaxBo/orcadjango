54a1b14fbc696fdb11ae273e5716fbca6bb05709
Christoph Franke <franke@ggr-planung.de>
2023-04-18 17:21:03 +0200

    replaced last migrations with one that also creates profiles for existing users

--- orcaserver/admin.py
+++ orcaserver/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .injectables import Injectable
-from .models import Scenario, Project
+from .models import Scenario, Project, Profile
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class ProjectAdmin(admin.ModelAdmin):
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Scenario)
 admin.site.register(Injectable)
+admin.site.register(Profile)
