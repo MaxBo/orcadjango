aed50fba95f636daf37dd884a12bf5a2d47def3c
Christoph Franke <franke@ggr-planung.de>
2023-08-22 18:39:12 +0200

    persisted favicon and title in django template

--- orcadjango/views.py
+++ orcadjango/views.py
@@ -1,5 +1,13 @@
 from django.views.generic.base import TemplateView
+from orcaserver.models import SiteSetting
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        site_settings = SiteSetting.load()
+        context['title'] = site_settings.title
+        context['favicon'] = site_settings.favicon.url
+        return context
