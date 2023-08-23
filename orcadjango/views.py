6367c1f8632dede470f67ac6e0c082d11de72d4e
Christoph Franke <franke@ggr-planung.de>
2023-08-23 12:07:53 +0200

    serve bundled frontend files with django

--- orcadjango/views.py
+++ orcadjango/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from orcaserver.models import SiteSetting
+from django.conf import settings
 
 
 class IndexView(TemplateView):
@@ -10,4 +11,5 @@ class IndexView(TemplateView):
         site_settings = SiteSetting.load()
         context['title'] = site_settings.title
         context['favicon'] = site_settings.favicon.url
+        context.update(settings.ANGULAR_RESOURCES)
         return context
